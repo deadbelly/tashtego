60ce78b8a8021d282bcfb840995a48d881631349
Boone Epstein <booneepstein@gmail.com>
2021-03-04 18:30:18 -0600

    Fix cover url

--- src/util.js
+++ src/util.js
@@ -17,6 +17,6 @@ export const formatBook = (bookData) => {
     authors: bookData.author_name,
     pubYear: bookData.first_publish_year,
     editions: bookData.edition_count,
-    cover: `https://covers.openlibrary.org/b/${bookData.cover_i}/1-S.jpg`
+    cover: `https://covers.openlibrary.org/b/id/${bookData.cover_i}-L.jpg`
   }
 }
