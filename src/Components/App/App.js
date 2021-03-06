bc8e272b648a1ed89a4c1fbe8615994df3d28781
Boone Epstein <booneepstein@gmail.com>
2021-03-06 11:51:36 -0600

    Fix addBook

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,23 +4,25 @@ import { Search } from '../Search/Search';
 import { NavBar } from '../NavBar/NavBar';
 import { ReadingList } from '../ReadingList/ReadingList';
 import { Route } from 'react-router-dom';
+import moment from 'moment';
 
 export const App = () => {
   const [readingList, setReadingList] = useState([]);
-  const [activeBook, setActiveBook] = useState(null);
+  const [activeBook, setActiveBook] = useState({id: null});
 
   useEffect(() => {
     if (readingList.length && !activeBook.id) {
       setActiveBook({
         ...readingList[0],
-        time: Date.now()
+        time: moment().add(15, 'days').calendar()
       })
       setReadingList(readingList.splice(1))
     }
   }, [readingList, activeBook])
 
   const addBook = newBook => {
-    if (!readingList.filter(book => book.id === newBook.id).length) {
+    if (!readingList.filter(book => book.id === newBook.id).length
+      && activeBook.id !== newBook.id) {
       setReadingList([ ...readingList, newBook])
     }
   };
