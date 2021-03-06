b5bd744a57991e43e5cea8ff4929949f4730a7ac
Boone Epstein <booneepstein@gmail.com>
2021-03-05 20:51:16 -0600

    Add rendering to ReadingList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { Search } from '../Search/Search';
 import { NavBar } from '../NavBar/NavBar';
+import { ReadingList } from '../ReadingList/ReadingList';
 import { Route } from 'react-router-dom';
 
 export const App = () => {
@@ -11,13 +12,15 @@ export const App = () => {
     if (!readingList.filter(book => book.id === newBook.id).length) {
       setReadingList([ ...readingList, newBook])
     }
-  }
-
+  };
 
   return (
     <div className="App">
       <Route exact path='/search' render={ () =>
         <Search addBook={addBook} />
+      }/>
+      <Route exact path='/list' render={ () =>
+        <ReadingList readingList={readingList} />
       }/>
       <NavBar />
     </div>
--- src/Components/ReadingList/ReadingList.js
+++ src/Components/ReadingList/ReadingList.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import 'ReadingList.css'
+import React from 'react';
+import './ReadingList.css';
 
-export default ReadingList = () => {
-  return true
+export const ReadingList = ({ readingList }) => {
+  const books = readingList.map(book => {
+    return (
+      <div className='list-item' key={book.id}>
+        <h2>{book.title}</h2>
+        <h3> by {book.authors[0]}</h3>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      {books}
+    </>
+  )
 }
