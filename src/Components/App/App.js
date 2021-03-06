6cb8ae475d697e7dbeb451050cb10596252a8117
Boone Epstein <booneepstein@gmail.com>
2021-03-06 11:38:32 -0600

    Install moment

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Search } from '../Search/Search';
 import { NavBar } from '../NavBar/NavBar';
@@ -7,6 +7,17 @@ import { Route } from 'react-router-dom';
 
 export const App = () => {
   const [readingList, setReadingList] = useState([]);
+  const [activeBook, setActiveBook] = useState(null);
+
+  useEffect(() => {
+    if (readingList.length && !activeBook.id) {
+      setActiveBook({
+        ...readingList[0],
+        time: Date.now()
+      })
+      setReadingList(readingList.splice(1))
+    }
+  }, [readingList, activeBook])
 
   const addBook = newBook => {
     if (!readingList.filter(book => book.id === newBook.id).length) {
